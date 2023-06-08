1529bf92717e383ec8082e17a634b20461a64838
dope66 <prince628@naver.com>
2023-06-08 16:24:51 +0900

    resultpage serverName + shortUrl

--- src/main/java/com/project/UrlJrr/controller/UrlMappingController.java
+++ src/main/java/com/project/UrlJrr/controller/UrlMappingController.java
@@ -2,9 +2,11 @@ package com.project.UrlJrr.controller;
 
 import com.project.UrlJrr.domain.UrlMappingDto;
 import com.project.UrlJrr.service.UrlMappingService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,11 +23,14 @@ public class UrlMappingController {
 
     @GetMapping("/")
     public String home(UrlMappingDto urlMappingDto) {
+
         return "index";
     }
 
     @PostMapping("/urlMapping")
-    public String convertUrl(@ModelAttribute("urlMappingDto") UrlMappingDto urlMappingDto, Model model) {
+    public String convertUrl(@ModelAttribute("urlMappingDto") UrlMappingDto urlMappingDto, HttpServletRequest request,Model model) {
+        String serverName =request.getServerName();
+        model.addAttribute("serverName",serverName);
         String originalUrl = urlMappingDto.getOriginalUrl();
         String shortUrl = urlMappingService.generateShortUrl(originalUrl);
         // 생성된 단축 URL을 모델에 추가

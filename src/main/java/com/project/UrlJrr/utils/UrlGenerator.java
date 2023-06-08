c55ecea43aae38f5554c2f58d744ae35a5c1a825
dope66 <prince628@naver.com>
2023-06-08 13:55:14 +0900

    shortUrl_lenght 8 fixed

--- src/main/java/com/project/UrlJrr/utils/UrlGenerator.java
+++ src/main/java/com/project/UrlJrr/utils/UrlGenerator.java
@@ -2,36 +2,26 @@ package com.project.UrlJrr.utils;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.math.BigInteger;
 import java.util.UUID;
+
 @Slf4j
 public class UrlGenerator {
     private static final String BASE62_CHARACTERS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     private static final int BASE62_RADIX = BASE62_CHARACTERS.length();
+    private static final int SHORT_URL_LENGTH = 8;
 
     public static String generateShortUrl() {
         UUID uuid = UUID.randomUUID();
-        long mostSignificantBits = uuid.getMostSignificantBits();
-        long leastSignificantBits = uuid.getLeastSignificantBits();
+        BigInteger uuidValue = new BigInteger(uuid.toString().replace("-", ""), 16);
         StringBuilder sb = new StringBuilder();
-        appendBase62EncodedValue(sb, mostSignificantBits);
-        appendBase62EncodedValue(sb, leastSignificantBits);
+        while (sb.length() < SHORT_URL_LENGTH) {
+            BigInteger[] quotientAndRemainder = uuidValue.divideAndRemainder(BigInteger.valueOf(BASE62_RADIX));
+            sb.insert(0, BASE62_CHARACTERS.charAt(quotientAndRemainder[1].intValue()));
+            uuidValue = quotientAndRemainder[0];
+        }
         return sb.toString();
     }
-
-    private static void appendBase62EncodedValue(StringBuilder sb, long value) {
-        if (value < 0) {
-            value = Math.abs(value); // 음수 값을 양수로 변환
-        }
-        if (value == 0) {
-            sb.insert(0, BASE62_CHARACTERS.charAt(0)); // 0번째 문자를 추가
-            return; // 반복문 실행하지 않고 종료
-        }
-        while (value > 0) {
-            sb.insert(0, BASE62_CHARACTERS.charAt((int) (value % BASE62_RADIX)));
-            value /= BASE62_RADIX;
-        }
-    }
-
 
     public static void main(String[] args) {
         String shortUrl = generateShortUrl();

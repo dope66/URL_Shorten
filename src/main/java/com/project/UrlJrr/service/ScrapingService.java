dd0aec86c5eeaafa8a0476e0733e7f3ef2c191b2
dope66 <prince628@naver.com>
2023-06-11 20:26:21 +0900

    TimerTask 15min

--- src/main/java/com/project/UrlJrr/service/ScrapingService.java
+++ src/main/java/com/project/UrlJrr/service/ScrapingService.java
@@ -3,6 +3,7 @@ package com.project.UrlJrr.service;
 
 import com.project.UrlJrr.domain.Scrap;
 import com.project.UrlJrr.repository.ScrapRepository;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Connection;
@@ -10,11 +11,19 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Slf4j
@@ -22,8 +31,32 @@ import java.util.List;
 public class ScrapingService {
     private final ScrapRepository scrapRepository;
 
+    @PostConstruct
+    public void startScraping(){
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                try{
+                    List<Scrap> scraps = ScrapSaram();
+                    processScrapResults(scraps);
+                }catch (IOException e){
+                    log.error("에러");
+                }
+
+            }
+        };
+        // crawl 주기 설정 (15분 마다)
+        long delay = 0; // 딜레이 설정
+        long period =60*15*1000;
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(task, delay, period);
+    }
+
+
+
     public List<Scrap> ScrapSaram() throws IOException {
         List<Scrap> scraps = new ArrayList<>();
+
         // 기본 url 설정
         String url = "https://www.saramin.co.kr/zf_user/search/recruit?search_area=main&search_done=y&search_optional_item=n&searchType=recently&searchword=%EA%B0%9C%EB%B0%9C%EC%9E%90&recruitPage=1&recruitSort=relation&recruitPageCount=30&inner_com_type=&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9%2C10&show_applied=&quick_apply=&except_read=&ai_head_hunting=";
         String articleUrlPrefix = "https://www.saramin.co.kr";
@@ -33,14 +66,14 @@ public class ScrapingService {
                 .timeout(10000);
 
         Document doc = connection.get();
-        Elements elements = doc.select("#recruit_info_list > div.content > div.item_recruit");
+        Elements elements = doc.select("#recruit_info_list > div.content > div");
         int count = elements.size(); // div.item_recruit의 개수
         System.out.println("div.item_recruit 개수: " + count);
 
         for (Element element : elements) {
-            String articleText = element.select("div.area_job > h2.job_tit").text();
-            String articleUrl = element.select("div.area_job > h2.job_tit > a").attr("href");
-            String company = element.select("div.area_corp > strong.corp_name > a").text();
+            String articleText = element.select("div.area_job > h2 > a").text();
+            String articleUrl = element.select("div.area_job > h2 > a").attr("href");
+            String company = element.select("div.area_corp > strong > a").text();
             articleUrl = articleUrlPrefix + articleUrl;
 
             // articleUrl에서 rec_idx 값을 추출
@@ -51,10 +84,13 @@ public class ScrapingService {
                 int recIdxEndIndex = articleUrl.indexOf("&", recIdxStartIndex);
                 if (recIdxEndIndex != -1) {
                     recIdx = articleUrl.substring(recIdxStartIndex + 8, recIdxEndIndex);
+
                 } else {
+
                     recIdx = articleUrl.substring(recIdxStartIndex + 8);
                 }
             }
+
             //rec_idx값이 이미 db에 저장된게 있는지 확인
             boolean isDuplicate = false;
             List<Scrap> existingScraps = scrapRepository.findAll();
@@ -81,6 +117,16 @@ public class ScrapingService {
         }
 
         return scraps;
+    }
+
+    public void processScrapResults(List<Scrap> scraps) {
+        for (Scrap scrap : scraps) {
+            // 원하는 작업 수행 (예: 콘솔에 출력)
+            System.out.println("aritlceText: " + scrap.getArticleText());
+            System.out.println("articleURL: " + scrap.getArticleUrl());
+            System.out.println("Company: " + scrap.getCompany());
+            System.out.println("--------------------------------------");
+        }
     }
 
 

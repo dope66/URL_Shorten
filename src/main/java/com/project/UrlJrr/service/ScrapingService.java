188b07da302f871526037c7f2c32c7f187fabdab
dope66 <prince628@naver.com>
2023-06-11 12:19:04 +0900

    Web crawl deduplication using rec_idx

--- src/main/java/com/project/UrlJrr/service/ScrapingService.java
+++ src/main/java/com/project/UrlJrr/service/ScrapingService.java
@@ -24,9 +24,10 @@ public class ScrapingService {
 
     public List<Scrap> ScrapSaram() throws IOException {
         List<Scrap> scraps = new ArrayList<>();
+        // 기본 url 설정
         String url = "https://www.saramin.co.kr/zf_user/search/recruit?search_area=main&search_done=y&search_optional_item=n&searchType=recently&searchword=%EA%B0%9C%EB%B0%9C%EC%9E%90&recruitPage=1&recruitSort=relation&recruitPageCount=30&inner_com_type=&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9%2C10&show_applied=&quick_apply=&except_read=&ai_head_hunting=";
         String articleUrlPrefix = "https://www.saramin.co.kr";
-
+        // Jsoup을 이용한 연결 설정
         Connection connection = Jsoup.connect(url)
                 .header("User-Agent", "Mozilla/5.0")
                 .timeout(10000);
@@ -42,18 +43,41 @@ public class ScrapingService {
             String company = element.select("div.area_corp > strong.corp_name > a").text();
             articleUrl = articleUrlPrefix + articleUrl;
 
-//          이미 존재하는 공고인지 확인
-            if (!scrapRepository.existsByArticleUrl(articleUrl)) {
-                Scrap scrap = new Scrap(null, articleText, articleUrl, company);
-                System.out.println("제목 : " + articleText);
-                System.out.println("링크 : " + articleUrl);
-                System.out.println("회사 : " + company);
-                System.out.println("=================================");
-                scraps.add(scrap);
-                scrapRepository.save(scrap);
-
+            // articleUrl에서 rec_idx 값을 추출
+            // 사람인 공고의 키값
+            String recIdx = "";
+            int recIdxStartIndex = articleUrl.indexOf("rec_idx=");
+            if (recIdxStartIndex != -1) {
+                int recIdxEndIndex = articleUrl.indexOf("&", recIdxStartIndex);
+                if (recIdxEndIndex != -1) {
+                    recIdx = articleUrl.substring(recIdxStartIndex + 8, recIdxEndIndex);
+                } else {
+                    recIdx = articleUrl.substring(recIdxStartIndex + 8);
+                }
             }
-
+            //rec_idx값이 이미 db에 저장된게 있는지 확인
+            boolean isDuplicate = false;
+            List<Scrap> existingScraps = scrapRepository.findAll();
+            for (Scrap existingScrap : existingScraps) {
+                String existingArticleUrl = existingScrap.getArticleUrl();
+                int existingRecIdxStartIndex = existingArticleUrl.indexOf("rec_idx=");
+                if (existingRecIdxStartIndex != -1) {
+                    int existingRecIdxEndIndex = existingArticleUrl.indexOf("&", existingRecIdxStartIndex);
+                    if (existingRecIdxEndIndex != -1) {
+                        String existingRecIdx = existingArticleUrl.substring(existingRecIdxStartIndex + 8, existingRecIdxEndIndex);
+                        if (existingRecIdx.equals(recIdx)) {
+                            isDuplicate = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            Scrap scrap = new Scrap(null, articleText, articleUrl, company);
+            scraps.add(scrap);
+            // 제외
+            if (!isDuplicate) {
+                scrapRepository.save(scrap);
+            }
         }
 
         return scraps;

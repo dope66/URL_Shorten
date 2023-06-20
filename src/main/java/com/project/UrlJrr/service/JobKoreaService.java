0f01a5490208fde6b16cdd9611011970961c331e
dope66 <prince628@naver.com>
2023-06-20 20:11:17 +0900

    job korea crawling

--- src/main/java/com/project/UrlJrr/service/JobKoreaService.java
+++ src/main/java/com/project/UrlJrr/service/JobKoreaService.java
@@ -1,6 +1,5 @@
 package com.project.UrlJrr.service;
 
-
 import com.project.UrlJrr.entity.Scrap;
 import com.project.UrlJrr.repository.ScrapRepository;
 import jakarta.annotation.PostConstruct;
@@ -14,11 +13,8 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
@@ -26,7 +22,8 @@ import java.util.*;
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class ScrapingService {
+public class JobKoreaService {
+
     private final ScrapRepository scrapRepository;
 
     @PostConstruct
@@ -34,31 +31,24 @@ public class ScrapingService {
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
-                try {
-                    List<Scrap> scraps = ScrapSaram();
-
-                } catch (IOException e) {
-                    log.error("에러");
-                }
+                List<Scrap> scraps = jobkoreaScrap();
 
             }
         };
-        // crawling 주기 설정 (15분 마다)
+        // crawling 주기 설정 (20분 마다)
         long delay = 0; // 딜레이 설정
-        long period = 60 * 15 * 1000;
+        long period = 60 * 20 * 1000;
         Timer timer = new Timer();
         timer.scheduleAtFixedRate(task, delay, period);
     }
 
-
-    public List<Scrap> ScrapSaram() throws IOException {
+    public List<Scrap> jobkoreaScrap() {
+        System.out.println("잡코리아 스크랩 시작");
         List<Scrap> scraps = new ArrayList<>();
 
         // 기본 url 설정
-        String url = "https://www.saramin.co.kr/zf_user/search/recruit?search_area=main&search_done=y&search_optional_item=n&searchType=recently&searchword=%EA%B0%9C%EB%B0%9C%EC%9E%90&recruitPage=1&recruitSort=relation&recruitPageCount=40&inner_com_type=&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9%2C10&show_applied=&quick_apply=&except_read=&ai_head_hunting=";
-        String articleUrlPrefix = "https://www.saramin.co.kr";
-
-
+        String url = "https://www.jobkorea.co.kr/Search/?stext=%EA%B0%9C%EB%B0%9C%EC%9E%90&tabType=recruit&Page_No=1";
+        String articleUrlPrefix = "https://www.jobkorea.co.kr/";
         // Jsoup을 이용한 연결 설정 http 2.0 설정 코드
         OkHttpClient client = new OkHttpClient.Builder()
                 .connectionSpecs(List.of(ConnectionSpec.COMPATIBLE_TLS, ConnectionSpec.CLEARTEXT))
@@ -69,7 +59,7 @@ public class ScrapingService {
             Request request = new Request.Builder()
                     .url(url)
                     .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36")
-                    .header("Referer", "https://www.saramin.co.kr/zf_user/jobs/relay/view?isMypage=no&rec_idx=45868854&recommend_ids=eJxVj8ERAzEIA6vJHwwS4p1Crv8u4lzmbOe5s8KyEkKz6tLwV70TGhwZl%2FyHVYJhoUhjznDfyHS6PTbnrQPLqgKxkU12HU9BoYUot7bd294YO4ygHdYsv994bFVIf9i9w6Nn2WF99B44d8e518Q8ijylu%2BgDiSFAdg%3D%3D&view_type=avatar&gz=1&t_ref_scnid=805&t_ref_content=SRI_100_MAIN_AI_RCT_LOGOUT&t_ref=main&t_category=relay_view&t_content=relay_view_avatar#seq=0")
+                    .header("Referer", "https://www.jobkorea.co.kr/Recruit/GI_Read/42052468?Oem_Code=C1&logpath=1&stext=%EA%B0%9C%EB%B0%9C%EC%9E%90&listno=1")
                     .build();
             Response response = client.newCall(request).execute();
             String html = response.body().string();
@@ -81,9 +71,10 @@ public class ScrapingService {
 
         }
 
-        Elements elements = doc.select("#recruit_info_list > div.content > div");
-        int count = elements.size(); // div.item_recruit의 개수
-        System.out.println("사람인 개수: " + count);
+        Elements elements = doc.select("#content > div > div > div.cnt-list-wrap > div >" +
+                " div.recruit-info > div.lists > div > div.list-default > ul > li");
+        int count = elements.size();
+        System.out.println("JObKorea 갯수 : " + count);
         /*
          * articleText  : 공고 제목
          * articleUrl : 공고 링크
@@ -95,59 +86,43 @@ public class ScrapingService {
          * jobType : 정규직 여부(?)
          * */
         for (Element element : elements) {
-            String articleText = element.select("div.area_job > h2 > a > span").text();
-            String articleUrl = element.select("div.area_job > h2 > a").attr("href");
-            Elements skillStackElements = element.select("div.job_sector > a");
+            String articleText = element.select("div.post-list-info > a.title dev_view on ").text();
+            String articleUrl = element.select("div.post-list-info > a").attr("href");
+            Elements skillStackElements = element.select("div.post-list-info > p.etc");
             StringJoiner skillStackJoiner = new StringJoiner(", ");
             for (Element skillStackElement : skillStackElements) {
                 String skillStack = skillStackElement.text();
                 skillStackJoiner.add(skillStack);
             }
             String skillStack = skillStackJoiner.toString();
-            String company = element.select("div.area_corp > strong > a").text();
-            String deadline = element.select("div.area_job > div.job_date > span.date").text();
-            String location = element.select("div.job_condition > span > a").text();
-            String experience = element.select("div.job_condition > span:nth-child(2)").text();
-            String requirement = element.select("div.job_condition > span:nth-child(3)").text();
-            String jobType = element.select("div.job_condition > span:nth-child(4)").text();
-
+            String company = element.select("div.post-list-corp > a.name dev_view ").text();
+            String deadline = element.select("div.post-list-info > p.option > span.date").text();
+            String location = element.select("div.post-list-info > p.option > span.loc long").text();
+            String experience = element.select("div.post-list-info > p.option > span.exp").text();
+            String requirement = element.select("div.post-list-info > p.option > span.edu").text();
+            String jobType = element.select("div.post-list-info > p.option > span:nth-child(3)").text();
 
             articleUrl = articleUrlPrefix + articleUrl;
 
-            // articleUrl에서 rec_idx 값을 추출
-            // 사람인 공고의 키값
-            String recIdx = "";
-            int recIdxStartIndex = articleUrl.indexOf("rec_idx=");
-            if (recIdxStartIndex != -1) {
-                int recIdxEndIndex = articleUrl.indexOf("&", recIdxStartIndex);
-                if (recIdxEndIndex != -1) {
-                    recIdx = articleUrl.substring(recIdxStartIndex + 8, recIdxEndIndex);
+            String urlKey = element.select("li.list-post").attr("data-gno");
+            System.out.println("JOb korea urlKey " + urlKey);
 
-                } else {
-
-                    recIdx = articleUrl.substring(recIdxStartIndex + 8);
-                }
-            }
-
-            //rec_idx값이 이미 db에 저장된게 있는지 확인
             boolean isDuplicate = false;
             List<Scrap> existingScraps = scrapRepository.findAll();
             for (Scrap existingScrap : existingScraps) {
                 String existingArticleUrl = existingScrap.getArticleUrl();
-                int existingRecIdxStartIndex = existingArticleUrl.indexOf("rec_idx=");
-                if (existingRecIdxStartIndex != -1) {
-                    int existingRecIdxEndIndex = existingArticleUrl.indexOf("&", existingRecIdxStartIndex);
-                    if (existingRecIdxEndIndex != -1) {
-                        String existingRecIdx = existingArticleUrl.substring(existingRecIdxStartIndex + 8, existingRecIdxEndIndex);
-                        if (existingRecIdx.equals(recIdx)) {
+                int existingStartUrlKey = existingArticleUrl.indexOf("data-gno");
+                if (existingStartUrlKey == -1) {
+                    int existingEndUrlKey = existingArticleUrl.indexOf("?", existingStartUrlKey);
+                    if (existingEndUrlKey != -1) {
+                        String existingUrlKey = existingArticleUrl.substring(existingStartUrlKey + 8, existingEndUrlKey);
+                        if (existingUrlKey.equals(urlKey)) {
                             isDuplicate = true;
                             break;
                         }
                     }
                 }
             }
-
-
             Scrap scrap = Scrap.builder()
                     .articleText(articleText)
                     .articleUrl(articleUrl)
@@ -161,23 +136,16 @@ public class ScrapingService {
                     .sent(false)
                     .createDate(String.valueOf(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"))))
                     .build();
-
             scraps.add(scrap);
-            // 제외
-            if (!isDuplicate) {
+
+            if(!isDuplicate){
                 scrapRepository.save(scrap);
             }
         }
 
+
         return scraps;
     }
 
-    public Page<Scrap> showListScrap(Pageable pageable) {
-        return scrapRepository.findAllByOrderByidDesc(pageable);
-    }
-
-
-
 
 }
-

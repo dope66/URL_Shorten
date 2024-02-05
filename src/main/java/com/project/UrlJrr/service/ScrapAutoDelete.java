6b86731921725a2dc4cd9101a53f6baeb81d2bb5
dope66 <prince628@naver.com>
2024-02-06 01:53:47 +0900

    자동삭제 기능 수정

--- src/main/java/com/project/UrlJrr/service/ScrapAutoDelete.java
+++ src/main/java/com/project/UrlJrr/service/ScrapAutoDelete.java
@@ -30,7 +30,7 @@ public class ScrapAutoDelete {
             String creatDate = scrap.getCreateDate();
             Pattern pattern = Pattern.compile("\\d{4}-\\d{2}-\\d{2}");
             Matcher matcher = pattern.matcher(deadline);
-            if (deadline.equals("채용시") || deadline.equals("상시채용")) {
+            if (deadline.equals("채용시") || deadline.equals("상시채용")||deadline.equals("진행예정")) {
 
                 int year = Integer.parseInt(creatDate.substring(0, 4));
                 int month = Integer.parseInt(creatDate.substring(5, 7));

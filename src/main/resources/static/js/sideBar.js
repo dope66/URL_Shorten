76b095c11b6ce3caaffc712df81660299489b69a
dope66 <prince628@naver.com>
2024-03-20 15:33:47 +0900

    내보내기 엑셀 파일 확장자 고정

--- src/main/resources/static/js/sideBar.js
+++ src/main/resources/static/js/sideBar.js
@@ -31,24 +31,6 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         window.location.href = `/mes/productionOrder?processName=${selectedProcessName}&equipmentName=${selectedEquipmentName}`;
     });
-
-    // 메뉴 토글 기능 개선
-    // document.querySelectorAll('.toggle-menu').forEach(function(menu) {
-    //     menu.addEventListener('click', function(e) {
-    //         e.preventDefault();
-    //         let targetMenu = e.currentTarget.nextElementSibling; // 현재 클릭된 요소의 바로 다음 요소 선택
-    //
-    //         // 모든 메뉴 숨김, 현재 메뉴는 제외
-    //         document.querySelectorAll('.side-menu').forEach(function(menu) {
-    //             if (menu !== targetMenu) {
-    //                 menu.style.display = 'none';
-    //             }
-    //         });
-    //
-    //         // 현재 메뉴 토글
-    //         targetMenu.style.display = targetMenu.style.display === 'block' ? 'none' : 'block';
-    //     });
-    // });
     document.querySelectorAll('.toggle-menu').forEach(function(menu) {
         menu.addEventListener('click', function(e) {
             e.preventDefault();
